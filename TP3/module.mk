9d2b6bccc45f83cb5b7e49c4432c8ff64ebc34dd
Solana <scaul@fi.uba.ar>
2020-06-29 12:01:20 -0300

    module mk

---
+++ TP3/module.mk
@@ -0,0 +1,28 @@
+ifeq ($(USE_SAPI),y)
+
+SAPI_BASE=libs/sapi/sapi_v0.5.2
+
+DEFINES+=USE_SAPI
+
+INCLUDES += -I$(SAPI_BASE)/base/inc
+INCLUDES += -I$(SAPI_BASE)/soc/core/inc
+INCLUDES += -I$(SAPI_BASE)/soc/peripherals/inc
+INCLUDES += -I$(SAPI_BASE)/soc/peripherals/usb/device/inc
+INCLUDES += -I$(SAPI_BASE)/soc/peripherals/usb/host/inc
+INCLUDES += -I$(SAPI_BASE)/board/inc
+INCLUDES += -I$(SAPI_BASE)/abstract_modules/inc
+INCLUDES += -I$(SAPI_BASE)/utilities/inc
+
+SRC += $(wildcard $(SAPI_BASE)/base/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/core/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/peripherals/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/peripherals/usb/device/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/soc/peripherals/usb/host/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/board/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/abstract_modules/src/*.c)
+SRC += $(wildcard $(SAPI_BASE)/utilities/src/*.c)
+
+# External Peripherals
+include $(SAPI_BASE)/external_peripherals/module.mk
+
+endif
